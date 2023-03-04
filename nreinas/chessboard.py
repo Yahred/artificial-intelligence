086a7edf758b6751ee4a7d950a52929a8e71dd65
Yahred <dr.yahred@outlook.es>
2023-03-04 15:42:37 -0700

    Se sube avance

--- nreinas/chessboard.py
+++ nreinas/chessboard.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+import os
+
+absolute_folder_path = os.path.dirname(os.path.realpath(__file__))
+absolute_image_path = os.path.join(absolute_folder_path, 'assets/reina.gif')
 
 class Queen:
     
@@ -7,22 +11,22 @@ class Queen:
         self.y = y
 
     def draw(self):
-        img = tk.PhotoImage('assets/reina.png')
+        img = tk.PhotoImage(file=absolute_image_path)
         return img
 
 class Chessboard:
 
-    def __init__(self, n_reinas: int = 4, reinas: list[Queen] = []) -> None:
+    def __init__(self, window: tk.Tk, n_reinas: int = 4, reinas: list[Queen] = []) -> None:
         self.n_reinas = n_reinas
         self.reinas = reinas
         self.queensize = 80
-        self.construir_campo()
+        self.construir_campo(window)
 
-    def construir_campo(self):
-        self.campo = tk.Canvas(width=(self.n_reinas*self.queensize), height=(self.n_reinas*self.queensize))
-    
+    def construir_campo(self, window: tk.Tk):
+        self.campo = tk.Canvas(window, width=(self.n_reinas*self.queensize), height=(self.n_reinas*self.queensize))
+
         for i in range(self.n_reinas):
-            fill_color_pattern = ['black', 'white'] if i % 2 == 0 else ['white', 'black']
+            fill_color_pattern = ['#b58863', '#f0d9b5'] if i % 2 == 0 else ['#f0d9b5', '#b58863']
 
             for j in range(self.n_reinas):
                 x1 = i * self.queensize
@@ -31,9 +35,12 @@ class Chessboard:
                 y2 = y1 + self.queensize
                 color = fill_color_pattern[j % 2 == 0]
                 self.campo.create_rectangle(x1, y1, x2, y2, fill=color)
+        img = tk.PhotoImage(file=absolute_image_path)
+        self.campo.create_image(100, 100, {'image': img})    
 
-    def add_reina(self, reina: Queen):
-        self.reinas.append(reina)
+    def add_reina(self):
+        img = tk.PhotoImage('reina.png')
+        self.campo.create_image(0, 50, {'image': img})
 
     def refresh(self):
         self.campo.update()
--- test_n_reinas.py
+++ test_n_reinas.py
@@ -2,9 +2,15 @@ import tkinter as tk
 from nreinas.chessboard import Chessboard
 
 window = tk.Tk()
-chessboard = Chessboard()
+chessboard = Chessboard(window)
 chessboard.pack()
 
+def add_queen():
+    chessboard.add_reina()
+    print('im not updating')
+    window.update_idletasks()
+
+window.after(2000, add_queen)
 window.mainloop()
 
 
